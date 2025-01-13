72b5dda5351896048b2ec6cb68fc4d8b6cb56d5a
yonraz <yonatan.raz@frontegg.com>
2025-01-13 21:10:39 +0200

    asd

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,12 @@ import { authenticator } from "./auth/authenticator";
 import { IAccessTokensOptions } from "@frontegg/client/dist/src/components/frontegg-context/types";
 
 dotenv.config();
-export const apiKey = process.env.FE_API_KEY;
-export const clientId = process.env.FE_CLIENT_ID;
-export const feDomain = process.env.FE_BASE_URL;
+export const API_KEY = process.env.FE_API_KEY;
+export const CLIENT_ID = process.env.FE_CLIENT_ID;
+export const FE_BASE_URL = process.env.FE_BASE_URL;
+export const FE_TENANT_JWT = process.env.FE_TENANT_JWT;
 
-console.log(`${apiKey} ${clientId}`);
+console.log(`${API_KEY} ${CLIENT_ID}`);
 
 const accessTokensOptions: IAccessTokensOptions = {
   cache: {
